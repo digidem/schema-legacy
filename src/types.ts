913326d943c7831bd8e7f473516b77897854de4b
Tomás Ciccola <tciccola@digital-democracy.com>
2024-07-04 12:21:35 -0300

    move unmigratable schemas to old, use `version` to type object for validation

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,11 @@
 import { type MapeoValue as AllMapeoValues } from './schema/index.js'
-export type SupportedSchemaNames = 'preset' | 'field' | 'observation'
+export type SupportedSchemaNames = 'observation'
 
 /** Filter a union of objects to only include those that have a prop `schemaName` that matches U */
 export type FilterBySchemaName<
-  T extends { schemaName: string },
+  T extends { version: string },
   U extends string,
-> = Extract<T, { schemaName: U }>
+> = Extract<T, { version: U }>
 
 export type MapeoValue = FilterBySchemaName<
   AllMapeoValues,
